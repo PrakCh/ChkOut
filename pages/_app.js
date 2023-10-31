b62ce00500a7550ab012269c9ac6b39c272930a5
Prakhar Chitravanshi <prakharchitravanshi@gmail.com>
2023-10-31 18:21:41 +0530

    Sanity Update

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,14 @@ import { Layout } from '../components';
 
 import { StateContext } from '@/context/StateContext';
 import { Toaster } from 'react-hot-toast';
+
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
         <Toaster/>
         <Component {...pageProps} />
-      </Layout>
+      </Layout> 
     </StateContext>
   )
 }
